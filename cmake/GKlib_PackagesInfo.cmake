cbd7597ae215475c1799aba063945ed96b9cac3f
Cristian Le <cristian.le@mpsd.mpg.de>
2023-10-26 17:58:42 +0200

    CMake project modernization
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

---
+++ cmake/GKlib_PackagesInfo.cmake
@@ -0,0 +1,12 @@
+set_package_properties(pcre2 PROPERTIES
+		URL https://github.com/PCRE2Project/pcre2
+		DESCRIPTION "Perl-Compatible Regular Expressions"
+		TYPE RECOMMENDED
+		PURPOSE "Regex parser"
+)
+set_package_properties(OpenMP PROPERTIES
+		URL https://www.openmp.org/resources/openmp-compilers-tools/
+		DESCRIPTION "The OpenMP API specification for parallel programming"
+		TYPE RECOMMENDED
+		PURPOSE "Parallelization library"
+)
