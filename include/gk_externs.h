dfdf1aad7537a555f48de3db9f55b3ccf6b57d77
Cristian Le <cristian.le@mpsd.mpg.de>
2023-10-27 12:44:48 +0200

    Use C standard `thread_local`
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- include/gk_externs.h
+++ include/gk_externs.h
@@ -10,15 +10,25 @@
 #ifndef _GK_EXTERNS_H_
 #define _GK_EXTERNS_H_
 
+// Windows does not support _Thread_local. Use appropriate aliases
+// Reference: https://stackoverflow.com/a/18298965
+#ifndef thread_local
+#if __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
+#define thread_local _Thread_local
+#elif defined _MSC_VER
+#define thread_local __declspec(thread)
+#elif defined __GNUC__
+#define thread_local __thread
+#else
+#error "Cannot define thread_local"
+#endif
+#endif
 
-/*************************************************************************
-* Extern variable definition. Hopefully, the __thread makes them thread-safe.
-**************************************************************************/
 #ifndef _GK_ERROR_C_
 /* declared in error.c */
-extern __thread int gk_cur_jbufs;
-extern __thread jmp_buf gk_jbufs[];
-extern __thread jmp_buf gk_jbuf;
+extern thread_local int gk_cur_jbufs;
+extern thread_local jmp_buf gk_jbufs[];
+extern thread_local jmp_buf gk_jbuf;
 
 #endif
 
--- src/memory.c
+++ src/memory.c
@@ -16,7 +16,7 @@ can be used to define other memory allocation routines.
 #include <GKlib.h>
 
 /* This is for the global mcore that tracks all heap allocations */
-static __thread gk_mcore_t *gkmcore = NULL;
+static thread_local gk_mcore_t *gkmcore = NULL;
 
 
 /*************************************************************************/
